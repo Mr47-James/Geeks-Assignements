bdee465b8ecb18e1920cc0e82794bb9cef42932c
Mr47-James <benlouazzani.hamza@gmail.com>
2025-09-19 06:21:54 +0100

    Create Exercise 1(Ninja).py

---
+++ WEAK_I/DAY-2/Exercise 1(Ninja).py
@@ -0,0 +1,26 @@
+X = "Volkswagen, Toyota, Ford Motor, Honda, Chevrolet, Toyota"
+Brand = X.split(",")
+Brand.sort(reverse=True)
+#Counter = X.count("o")
+crem = 0
+icrem = 0
+ical = len(Brand) - icrem 
+nana = set(Brand)
+for i in Brand:
+    if i.find("o"):
+        crem += 1
+    if i.find("i"):
+        icrem += 1
+print(f'[*] Brand With No Duplicates: "{", ".join(set(Brand))}"')
+print(f'[*] Brand Sorted: "{", ".join(sorted(set(Brand)))}"')
+print(f'[*] Brand fully sorted: "{", ".join(b[::-1] for b in set(Brand))}"')
+
+
+print(f"[*] Number of Letter <O>: {X.count('o')}")    
+print(f"[*] Number of Manufacturer's Names with <o>: {crem}")
+print(f"Number of Letter <i>: {X.count('i')}")    
+print(f"Number of Manufacturer's Names without <i>: {ical}")
+print(f"[*] Brands List: {Brand}")
+print(f"[*] Total Number of Manufacturers:  {len(Brand)}")
+
+
