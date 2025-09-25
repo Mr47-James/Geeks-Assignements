762f8ab4bce7f79723df06836f7949ed8ad1fbee
Mr47-James <benlouazzani.hamza@gmail.com>
2025-09-25 16:58:44 +0100

    Update Exercise 1(Ninja).py

--- WEAK_I/DAY-3/exercise_xp_ninja/Exercise 1(Ninja).py
+++ WEAK_I/DAY-3/exercise_xp_ninja/Exercise 1(Ninja).py
@@ -1,39 +1,106 @@
 class Phone:
-    def __init__(self, phone_number, call_history, messages):
-        self.pn = phone_number
+    def __init__(self, phone_number):
+        self.phone_number = phone_number
         self.call_history = []
         self.messages = []
+
     def call(self, other_phone):
-        print(f"[*] Caller ID: {other_phone}")
-        self.call_history.append(other_phone)
+        log = f"{self.phone_number} called {other_phone.phone_number}"
+        print(log)
+        self.call_history.append(log)
+
     def show_call_history(self):
-        print(f"[*] Printing Call-History:\n{', '.join(self.call_history)}")
-    def send_message(self):
-        XMT = input("[+] Put in your Message Destinator ID: ")
-        XMF = input("[+] Put in your ID: ")
-        XMC = input("[+} Put in your Message: ")
-        self.messages = list(self.XMH.keys())
-        Last_Message = self.messages[-1]
-        for i in Last_Message:
-            if i.isdigit():
-                num += i
-        if num:
-            Prefix = int(num)
-            Prefix += 1
-            key = f"Message{num}"
-        XMH[key]["to"] = XMT 
-        XMH[key]["from"] = XMF
-        XMH[key]["content"] = XMC
-        return XMH
+        print("[*] Call History:")
+        if not self.call_history:
+            print("   (empty)")
+        for call in self.call_history:
+            print("  ", call)
+
+    def send_message(self, to_phone, content):
+        message = {
+            "to": to_phone.phone_number,
+            "from": self.phone_number,
+            "content": content
+        }
+        self.messages.append(message)
+        to_phone.messages.append(message)
+
     def show_outgoing_messages(self):
-        print(f"[*] Printing Outgoing Messages:\n{'\n'.join(self.messages[-1])}")
-    #def show_incoming_messages(self):
-Mode = intput(f"1. Call\n2. Call-History\n3. Send-Message\n4. Outgoing-Messages\n[?] Choose you mode: ")
-match Mode
-    case "1":
-        DID = input("[?] Your Destination Id:")
-        
-        
-        
-            
-            
+        print("[*] Outgoing Messages:")
+        found = False
+        for msg in self.messages:
+            if msg["from"] == self.phone_number:
+                print(msg)
+                found = True
+        if not found:
+            print("   (none)")
+
+    def show_incoming_messages(self):
+        print("[*] Incoming Messages:")
+        found = False
+        for msg in self.messages:
+            if msg["to"] == self.phone_number:
+                print(msg)
+                found = True
+        if not found:
+            print("   (none)")
+
+    def show_messages_from(self, other_number):
+        print(f"[*] Messages from {other_number}:")
+        found = False
+        for msg in self.messages:
+            if msg["from"] == other_number and msg["to"] == self.phone_number:
+                print(msg)
+                found = True
+        if not found:
+            print("   (none)")
+
+
+p1 = Phone("111-111")
+p2 = Phone("222-222")
+p3 = Phone("333-333")
+
+while True:
+    print("\n--- PHONE MENU ---")
+    print("1. Make a Call")
+    print("2. Show Call History")
+    print("3. Send Message")
+    print("4. Show Outgoing Messages")
+    print("5. Show Incoming Messages")
+    print("6. Show Messages From Specific Number")
+    print("0. Exit")
+    mode = input("[?] Choose your mode: ")
+
+    match mode:
+        case "1":
+            dest = input("[?] Enter destination (222-222 or 333-333): ")
+            if dest == p2.phone_number:
+                p1.call(p2)
+            elif dest == p3.phone_number:
+                p1.call(p3)
+            else:
+                print("[!] Unknown destination.")
+        case "2":
+            p1.show_call_history()
+        case "3":
+            dest = input("[?] Enter destination (222-222 or 333-333): ")
+            msg = input("[+] Enter your message: ")
+            if dest == p2.phone_number:
+                p1.send_message(p2, msg)
+            elif dest == p3.phone_number:
+                p1.send_message(p3, msg)
+            else:
+                print("[!] Unknown destination.")
+        case "4":
+            p1.show_outgoing_messages()
+        case "5":
+            p1.show_incoming_messages()
+        case "6":
+            sender = input("[?] Enter sender number: ")
+            p1.show_messages_from(sender)
+        case "0":
+            print("[*] Exiting... Returning '00'")
+            print("00")
+            break
+        case _:
+            print("[!] Invalid choice, try again.")
