40c9132a66e52244a7a0e9f8fea3dc14c239dc32
Mr47-James <benlouazzani.hamza@gmail.com>
2025-09-27 22:15:09 +0100

    Create Exercise 3.py

---
+++ WEAK_I/DAY-4/Exercises_xp/Exercise 3.py
@@ -0,0 +1,28 @@
+import random
+from dog import Dog
+
+class PetDog(Dog):
+    def __init__(self, name, age, weight, trained=False):
+        super().__init__(name, age, weight)
+        self.trained = trained
+
+    def train(self):
+        print(self.bark())
+        self.trained = True
+
+    def play(self, *dog_names):
+       
+        all_dogs = ", ".join(dog_names)
+        print(f"{self.name}, {all_dogs} all play together")
+
+    def do_a_trick(self):
+        if self.trained:
+            tricks = [
+                f"{self.name} does a barrel roll",
+                f"{self.name} stands on his back legs",
+                f"{self.name} shakes your hand",
+                f"{self.name} plays dead"
+            ]
+            print(random.choice(tricks))
+        else:
+            print(f"{self.name} is not trained yet and can't do tricks.")
