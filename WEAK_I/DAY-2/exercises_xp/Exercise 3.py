ac787a4a780fae4d238196bf7996a2981e3fb4ed
Mr47-James <benlouazzani.hamza@gmail.com>
2025-09-27 16:10:40 +0100

    Update Exercise 3.py

--- WEAK_I/DAY-2/exercises_xp/Exercise 3.py
+++ WEAK_I/DAY-2/exercises_xp/Exercise 3.py
@@ -1,22 +1,42 @@
-brand = dict(
-    name="Zara",
-    create_date=1975,
-    creator_name="Amancio Ortega Gaona",
-    type_of_clothes=["men", "women", "children", "home"],
-    international_competitors=["Gap", "H&M", "Benetton"],
-    number_stores=7000,
-    major_color={"France": "blue", "Spain": "red", "US": ["pink", "green"]}
-)
+brand = {
+    "name": "Zara",
+    "creation_date": 1975,
+    "creator_name": "Amancio Ortega Gaona",
+    "type_of_clothes": ["men", "women", "children", "home"],
+    "international_competitors": ["Gap", "H&M", "Benetton"],
+    "number_stores": 7000,
+    "major_color": {
+        "France": "blue",
+        "Spain": "red",
+        "US": ["pink", "green"]
+    }
+}
 
 brand["number_stores"] = 2
-print(f"The Zara Targeted Clients Are {brand.get('type_of_clothes')}")
+
+print(f"Zara's clients are: {', '.join(brand['type_of_clothes'])}.")
 
 brand["country_creation"] = "Spain"
 
-if "international_competitors" in brand:   
-    brand["international_competitors"] = "Desigual"
-    brand.pop("create_date")  
-    print(brand["international_competitors"])
-    print(brand.keys())
-    brand["more_on_zara"] = {"creation_date": 1975, "number_stores": 10000}
-    print(brand.get("number_stores"))
+if "international_competitors" in brand:
+    brand["international_competitors"].append("Desigual")
+
+del brand["creation_date"]
+
+print("Last competitor:", brand["international_competitors"][-1])
+
+print("US major colors:", brand["major_color"]["US"])
+
+
+print("Number of key-value pairs:", len(brand))
+
+print("Keys:", brand.keys())
+
+more_on_zara = {
+    "creation_date": 1975,
+    "number_stores": 10000
+}
+
+brand.update(more_on_zara)
+
+print("Updated number of stores:", brand["number_stores"])
