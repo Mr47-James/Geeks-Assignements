e2dee3ae8e984088eeae9f8f9c2d6bd487d3d4cc
Mr47-James <benlouazzani.hamza@gmail.com>
2025-09-28 21:30:50 +0100

    Create Exercise 1.sql

---
+++ WEEK2/DAY-2/exercises_xp/Exercise 1.sql
@@ -0,0 +1,21 @@
+-- 1. All items, ordered by price (lowest to highest)
+SELECT *
+FROM items
+ORDER BY price ASC;
+
+-- 2. Items with a price above or equal to 80, ordered by price (highest to lowest)
+SELECT *
+FROM items
+WHERE price >= 80
+ORDER BY price DESC;
+
+-- 3. First 3 customers in alphabetical order of first name (exclude id)
+SELECT first_name, last_name
+FROM customers
+ORDER BY first_name ASC
+LIMIT 3;
+
+-- 4. All last names only, in reverse alphabetical order (Z-A)
+SELECT last_name
+FROM customers
+ORDER BY last_name DESC;
