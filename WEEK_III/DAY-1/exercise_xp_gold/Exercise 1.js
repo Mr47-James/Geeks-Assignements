d6dd90e1262d4b439f4048542db8f1c99e099744
Mr47-James <benlouazzani.hamza@gmail.com>
2025-10-23 20:11:22 +0100

    Create Exercise 1.js

---
+++ WEEK_III/DAY-1/exercise_xp_gold/Exercise 1.js
@@ -0,0 +1,8 @@
+// ==========================
+// EXERCISE 1 - DIVISIBLE BY THREE
+// ==========================
+let numbers = [123, 8409, 100053, 333333333, 7];
+
+for (let num of numbers) {
+    console.log(num % 3 === 0);
+}
