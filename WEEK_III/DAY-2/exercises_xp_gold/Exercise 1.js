94b95c24064a86eade620a0ca5b0e290a13bc332
Mr47-James <benlouazzani.hamza@gmail.com>
2025-10-23 20:31:58 +0100

    Create Exercise 1.js

---
+++ WEEK_III/DAY-2/exercises_xp_gold/Exercise 1.js
@@ -0,0 +1,10 @@
+// ==========================
+// EXERCISE 1 - IS_BLANK
+// ==========================
+function isBlank(str) {
+    return str === "";
+}
+
+// Test cases
+console.log(isBlank(''));     // true
+console.log(isBlank('abc'));  // false
