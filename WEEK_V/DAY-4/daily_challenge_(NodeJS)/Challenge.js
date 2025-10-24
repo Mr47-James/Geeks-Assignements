94a6bdf08620bcbe1026dbdbb6f528195b7e8604
Mr47-James <benlouazzani.hamza@gmail.com>
2025-10-25 00:50:26 +0100

    Create Challenge.js

---
+++ WEEK_V/DAY-4/daily_challenge_(NodeJS)/Challenge.js
@@ -0,0 +1,13 @@
+// challenge.js
+const greet = require('./greeting');
+const showMessage = require('./colorful-message');
+const readFile = require('./read-file');
+
+// Greet the user
+console.log(greet('Elie'));
+
+// Show colorful message
+showMessage();
+
+// Read and display file content
+readFile();
