cffa3b59ef25c2a14110853da962eaae297a1f5a
Mr47-James <benlouazzani.hamza@gmail.com>
2025-10-24 22:04:12 +0100

    Delete WEEK_IV/DAY-2/exercises_xp_gold directory

--- WEEK_IV/DAY-2/exercises_xp_gold/Exercise 5.js
+++
@@ -1,5 +0,0 @@
-const epic = ['a', 'long', 'time', 'ago', 'in a', 'galaxy', 'far far', 'away'];
-
-// Use reduce to combine all elements into a single string
-const sentence = epic.reduce((acc, word) => acc + ' ' + word);
-console.log(sentence);
