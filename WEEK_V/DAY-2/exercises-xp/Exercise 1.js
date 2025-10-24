5ee9294c834da482719e745bacac40831fcb1601
Mr47-James <benlouazzani.hamza@gmail.com>
2025-10-24 23:03:09 +0100

    Create Exercise 1.js

---
+++ WEEK_V/DAY-2/exercises-xp/Exercise 1.js
@@ -0,0 +1,15 @@
+const apiUrl = "https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My";
+
+fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log("Giphy API Response:", data);
+  })
+  .catch(error => {
+    console.error("Error fetching data:", error);
+  });
