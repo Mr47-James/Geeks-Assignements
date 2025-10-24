d41facb32122facce2195a47b4589fc37acb4896
Mr47-James <benlouazzani.hamza@gmail.com>
2025-10-25 00:21:20 +0100

    Create math.js

---
+++ WEEK_V/DAY-4/exercises_xp/Exercise 5/math.js
@@ -0,0 +1,17 @@
+// math.js
+
+// Custom math module
+
+function add(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+// Export the functions
+module.exports = {
+  add,
+  multiply
+};
