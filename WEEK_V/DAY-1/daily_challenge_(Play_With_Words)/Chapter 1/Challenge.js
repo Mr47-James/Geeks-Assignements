4a69199ba9617116ca1ae2c58d1ce0b842a7c39c
Mr47-James <benlouazzani.hamza@gmail.com>
2025-10-24 22:59:23 +0100

    Create Challenge.js

---
+++ WEEK_V/DAY-1/daily_challenge_(Play_With_Words)/Chapter 1/Challenge.js
@@ -0,0 +1,40 @@
+// Function to capitalize all words
+function makeAllCaps(words) {
+  return new Promise((resolve, reject) => {
+    if (words.every(word => typeof word === "string")) {
+      resolve(words.map(word => word.toUpperCase()));
+    } else {
+      reject("Error: Not all elements are strings!");
+    }
+  });
+}
+
+// Function to sort words if array length > 4
+function sortWords(words) {
+  return new Promise((resolve, reject) => {
+    if (words.length > 4) {
+      resolve(words.sort());
+    } else {
+      reject("Error: Array length is not greater than 4!");
+    }
+  });
+}
+
+
+// Case 1: contains a number → catch executed
+makeAllCaps([1, "pear", "banana"])
+  .then(arr => sortWords(arr))
+  .then(result => console.log(result))
+  .catch(error => console.log(error)); 
+
+// Case 2: length not bigger than 4 → catch executed
+makeAllCaps(["apple", "pear", "banana"])
+  .then(arr => sortWords(arr))
+  .then(result => console.log(result))
+  .catch(error => console.log(error));
+
+// Case 3: valid → should log uppercased & sorted array
+makeAllCaps(["apple", "pear", "banana", "melon", "kiwi"])
+  .then(arr => sortWords(arr))
+  .then(result => console.log(result))
+  .catch(error => console.log(error));
