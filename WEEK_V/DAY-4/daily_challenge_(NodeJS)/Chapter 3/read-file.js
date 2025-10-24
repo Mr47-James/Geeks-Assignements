c9bbe5f5fa93fb03f00d9d71bf56337e11c1456f
Mr47-James <benlouazzani.hamza@gmail.com>
2025-10-25 00:49:18 +0100

    Create read-file.js

---
+++ WEEK_V/DAY-4/daily_challenge_(NodeJS)/Chapter 3/read-file.js
@@ -0,0 +1,16 @@
+// read-file.js
+const fs = require('fs');
+const path = require('path');
+
+function readFile() {
+  const filePath = path.join(__dirname, 'files', 'file-data.txt');
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading file:', err);
+      return;
+    }
+    console.log(data);
+  });
+}
+
+module.exports = readFile;
