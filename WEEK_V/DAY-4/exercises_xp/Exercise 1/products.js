43a1b41b801a423ae71b4f6f565a52a21e967ee3
Mr47-James <benlouazzani.hamza@gmail.com>
2025-10-24 23:20:20 +0100

    Create products.js

---
+++ WEEK_V/DAY-4/exercises_xp/Exercise 1/products.js
@@ -0,0 +1,13 @@
+// products.js
+
+// Array of product objects
+const products = [
+  { name: "Laptop", price: 1200, category: "Electronics" },
+  { name: "Shoes", price: 80, category: "Fashion" },
+  { name: "Coffee Mug", price: 15, category: "Kitchen" },
+  { name: "Headphones", price: 150, category: "Electronics" },
+  { name: "T-shirt", price: 25, category: "Fashion" },
+];
+
+// Export the array using CommonJS
+module.exports = products;
