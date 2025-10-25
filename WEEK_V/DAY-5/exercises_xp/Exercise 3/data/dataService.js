3a9a3a1aa758af60bc279c6357189d7556a9c3c5
Mr47-James <benlouazzani.hamza@gmail.com>
2025-10-25 01:11:51 +0100

    Create dataService.js

---
+++ WEEK_V/DAY-5/exercises_xp/Exercise 3/data/dataService.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+
+// Function to fetch posts from JSONPlaceholder
+const fetchPosts = async () => {
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    return response.data;
+  } catch (error) {
+    throw new Error('Error fetching posts: ' + error.message);
+  }
+};
+
+module.exports = { fetchPosts };
